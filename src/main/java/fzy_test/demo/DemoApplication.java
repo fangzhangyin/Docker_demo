b09beb82f27730923bf998dafed30623a8f6c30b
fangzhangyin <2570413931@qq.com>
2021-07-09 11:46:23 +0800

    配置文件，设置启动类 将项目打成war包

--- src/main/java/fzy_test/demo/DemoApplication.java
+++ src/main/java/fzy_test/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package fzy_test.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    //    重写方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return super.configure(builder);
+        return builder.sources(DemoApplication.class);
+    }
 }
